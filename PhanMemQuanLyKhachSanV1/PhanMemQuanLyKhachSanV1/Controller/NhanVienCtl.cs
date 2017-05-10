30a5098159a80d21bb2058dab0669876f514dea3
manhhiep1996 <manhhiep1996@gmail.com>
2017-05-11 01:08:18 +0700

    [Hiệp] hoàn thành form quản lí nhân viên

---
+++ PhanMemQuanLyKhachSanV1/PhanMemQuanLyKhachSanV1/Controller/NhanVienCtl.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhanMemQuanLyKhachSanV1.Model;
+using System.Data;
+
+namespace PhanMemQuanLyKhachSanV1.Controller
+{
+    class NhanVienCtl
+    {
+        ConnectToSql con = new ConnectToSql();
+        SqlCommand cmd = new SqlCommand();
+
+        /// <summary>
+        /// Hàm lấy dữ liệu . Trả về 1 data table
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from NhanVien";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                con.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// Hàm Thêm nhân viên vào danh sách
+        /// </summary>
+        /// <param name="nvobj">đối tượng cần thêm vào ds</param>
+        public bool AddNhanVien(NhanVienObj nvobj)
+        {
+            cmd.CommandText = "Insert into NhanVien values ('" + nvobj.MaNV + "',N'" + nvobj.TenNhanVien + "','" + nvobj.MaChucVu + "','" + nvobj.GioiTinh + "','" + nvobj.NgaySinh + "','" + nvobj.DiaChi + "','" + nvobj.SDT + "');";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Hàm xóa một nhân viên ra khỏi danh sách
+        /// </summary>
+        /// <param name="ma"> mã nhân viên cần xóa</param>
+        public bool DelNhanVien(string ma)
+        {
+            cmd.CommandText = "delete KhachHang where MaKH= '" + ma + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Hàm sửa thông tin một nhân viên
+        /// </summary>
+        /// <param name="khobj"> đối tượng nhân viên cần sửa</param>
+        public bool UpdateNhanVien(NhanVienObj nvobj)
+        {
+            cmd.CommandText = " update NhanVien set TenNhanVien=N'" + nvobj.TenNhanVien + "',MaChucVu='" + nvobj.MaChucVu + "',GioiTinh='" + nvobj.GioiTinh + "',NgaySinh='" + nvobj.NgaySinh + "',DiaChi='" + nvobj.DiaChi + "',SDT='" + nvobj.SDT + "' where MaNV='" + nvobj.MaNV + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+    }
+}
--- PhanMemQuanLyKhachSanV1/PhanMemQuanLyKhachSanV1/View/frmNhanVien.cs
+++ PhanMemQuanLyKhachSanV1/PhanMemQuanLyKhachSanV1/View/frmNhanVien.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using PhanMemQuanLyKhachSanV1.Model;
+using PhanMemQuanLyKhachSanV1.Controller;
 
 namespace PhanMemQuanLyKhachSanV1.View
 {
@@ -17,39 +20,154 @@ namespace PhanMemQuanLyKhachSanV1.View
             InitializeComponent();
         }
 
+        NhanVienCtl nvctl = new NhanVienCtl();
+        NhanVienObj nvobj = new NhanVienObj();
+        int flag = 0;
+        public void dis_en(bool e)
+        {
+            txtMaNV.Enabled = e;
+            txtHoTen.Enabled = e;
+            dtNgaySinh.Enabled = e;
+            txtDiaChi.Enabled = e;
+            groupBox5.Enabled = e;
+            cbbMaCV.Enabled = e;
+            txtSDT.Enabled = e;
+            btnHuy.Enabled = e;
+            btnLuu.Enabled = e;
+            btnThem.Enabled = !e;
+            btnSua.Enabled = !e;
+            btnXoa.Enabled = !e;
+        }
+        private void clean()
+        {
+            txtMaNV.Clear();
+            txtHoTen.Clear();
+            txtSDT.Clear();
+            txtDiaChi.Clear();
+            dtNgaySinh.Value = DateTime.Now;
+        }
+        private void GanDuLieu(NhanVienObj nv1obj)
+        {
+            nv1obj.MaNV = txtMaNV.Text.ToString().Trim();
+            nv1obj.TenNhanVien = txtHoTen.Text.ToString().Trim();
+            nv1obj.MaChucVu = cbbMaCV.Text.ToString().Trim();
+            nv1obj.NgaySinh = dtNgaySinh.Value;
+            nv1obj.DiaChi = txtDiaChi.Text.ToString().Trim();
+            if (rbtNam.Checked)
+            {
+                nv1obj.GioiTinh = "Nam";
+            }
+            if (rbtNu.Checked)
+            {
+                nv1obj.GioiTinh = "Nu";
+            }
+            nv1obj.SDT = txtSDT.Text.ToString().Trim();
+        }
         private void frmNhanVien_Load(object sender, EventArgs e)
         {
-
+            dgvNhanVien.DataSource = nvctl.GetData();
+            dgvNhanVien.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dis_en(false);
         }
 
         private void dgvNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                try
+                {
+                    txtMaNV.Text = dgvNhanVien.CurrentRow.Cells[0].Value.ToString();
+                    txtHoTen.Text = dgvNhanVien.CurrentRow.Cells[1].Value.ToString();
+                    cbbMaCV.Text = dgvNhanVien.CurrentRow.Cells[2].Value.ToString();
+                    bool Gt;
+                    if (dgvNhanVien.CurrentRow.Cells[3].Value.ToString().Trim() == "Nam")
+                        Gt = true;
+                    else
+                        Gt = false;
+                    if (Gt)
+                        rbtNam.Checked = true;
+                    else
+                        rbtNu.Checked = true;
+                    dtNgaySinh.Text = dgvNhanVien.CurrentRow.Cells[4].Value.ToString();
+                    txtDiaChi.Text = dgvNhanVien.CurrentRow.Cells[5].Value.ToString();
+                    txtSDT.Text = dgvNhanVien.CurrentRow.Cells[6].Value.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
 
         private void btnThem_Click(object sender, EventArgs e)
         {
-
+            flag = 0;
+            clean();
+            dis_en(true);
         }
 
         private void btnSua_Click(object sender, EventArgs e)
         {
-
+            flag = 1;
+            dis_en(true);
         }
 
         private void btnXoa_Click(object sender, EventArgs e)
         {
-
+            DialogResult dr = MessageBox.Show("Bạn có thật sự muốn xóa ?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            {
+                if (dr == DialogResult.Yes)
+                {
+                    if (nvctl.DelNhanVien(txtMaNV.Text.Trim()))
+                    {
+                        MessageBox.Show("Xóa thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        frmNhanVien_Load(sender, e);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Xóakhông thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
         }
 
         private void btnLuu_Click(object sender, EventArgs e)
         {
-
+            GanDuLieu(nvobj);
+            if (flag == 0)   // thêm
+            {
+                if (nvctl.AddNhanVien(nvobj))
+                {
+                    MessageBox.Show("Thêm thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    frmNhanVien_Load(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("Thêm không thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else            // sửa
+            {
+                if (nvctl.UpdateNhanVien(nvobj))
+                {
+                    MessageBox.Show("Sửa thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    frmNhanVien_Load(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("Sửa không thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void btnHuy_Click(object sender, EventArgs e)
         {
-
+            frmNhanVien_Load(sender, e);
+            dis_en(false);
         }
     }
 }
