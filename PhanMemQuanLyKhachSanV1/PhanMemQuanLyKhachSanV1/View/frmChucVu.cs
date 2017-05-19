973714a4037b04bc738664a55933266936ef5e0c
phongtung96 <phongtung96@gmail.com>
2017-05-19 21:32:25 +0700

    [PhongTung]PhanMemQuanLyKhacSan

--- PhanMemQuanLyKhachSanV1/PhanMemQuanLyKhachSanV1/View/frmChucVu.cs
+++ PhanMemQuanLyKhachSanV1/PhanMemQuanLyKhachSanV1/View/frmChucVu.cs
@@ -7,14 +7,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
+using PhanMemQuanLyKhachSanV1.Model;
+using PhanMemQuanLyKhachSanV1.Controller;
 namespace PhanMemQuanLyKhachSanV1.View
 {
     public partial class frmChucVu : Form
     {
+        ChucVuCtl cvctl = new ChucVuCtl();
+        ChucVuObj cvobj = new ChucVuObj();
+        int flag = 0;
         public frmChucVu()
         {
             InitializeComponent();
         }
+
+        private void cbbMaCV_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox5_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtNgaySinh_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSDT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDiaChi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmChucVu_Load(object sender, EventArgs e)
+        {
+          
+        }
+     
+
+       
+
+      
+
+        private void dgvChucVu_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void GanDuLieu(ChucVuObj cv1obj)
+        {
+          
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+           
+        }
+
     }
 }
--- PhanMemQuanLyKhachSanV1/PhanMemQuanLyKhachSanV1/View/frmMain.cs
+++ PhanMemQuanLyKhachSanV1/PhanMemQuanLyKhachSanV1/View/frmMain.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using PhanMemQuanLyKhachSanV1.View;
 namespace PhanMemQuanLyKhachSanV1.View
 {
     public partial class frmMain : Form
@@ -16,5 +16,57 @@ namespace PhanMemQuanLyKhachSanV1.View
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            frmPhong phong= new frmPhong();
+            phong.Show();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            frmNhanVien nv = new frmNhanVien();
+            nv.Show();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            frmPhieuDichVu phieudv = new frmPhieuDichVu();
+            phieudv.Show();
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            frmHoaDon hoadon = new frmHoaDon();
+            hoadon.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            KhachHang kh = new KhachHang();
+            kh.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            frmPhanLoaiPhong pl = new frmPhanLoaiPhong();
+            pl.Show();
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            frmChucVu cv = new frmChucVu();
+            cv.Show();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn có thật sự muốn thoát !", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
     }
 }
