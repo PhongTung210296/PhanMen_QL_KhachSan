5a350abcca106ecd4facd76645cee315e4d19eca
manhhiep1996 <manhhiep1996@gmail.com>
2017-05-23 14:49:47 -0700

    [Hiep] form tìm kiếm

---
+++ PhanMemQuanLyKhachSanV1/PhanMemQuanLyKhachSanV1/Controller/SearchCtl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhanMemQuanLyKhachSanV1.Model;
+using System.Data.SqlClient;
+using System.Data;
+namespace PhanMemQuanLyKhachSanV1.Controller
+{
+    class SearchCtl
+    {
+        ConnectToSql con = new ConnectToSql();
+        SqlCommand cmd = new SqlCommand();
+
+        public bool SeachKhachHang(KhachHangObj khobj)
+        {
+            cmd.CommandText = "select from KhachHang where TenKH LIKE " + khobj.TenKH + " ";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+    }
+}
